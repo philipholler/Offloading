b20ce7850bba9126597e4312b65e54589abc9a81
magnus <magnjensen@gmail.com>
2020-10-29 09:42:23 +0100

    Added a get request for downloading files.

--- src/main/java/p7gruppe/p7/offloading/controller/JobsController.java
+++ src/main/java/p7gruppe/p7/offloading/controller/JobsController.java
@@ -1,20 +1,36 @@
-package p7gruppe.p7.offloading.api;
+package p7gruppe.p7.offloading.controller;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.context.request.NativeWebRequest;
+import p7gruppe.p7.offloading.api.JobsApi;
 import p7gruppe.p7.offloading.model.Job;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
-import java.util.List;
-import java.util.Optional;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 @Controller
 public class JobsController implements JobsApi {
+
     @Override
-    public Optional<NativeWebRequest> getRequest() {
-        System.out.println("Got get request");
-        return Optional.empty();
+    public ResponseEntity<Resource> jobsGet(@NotNull @Valid Job name) {
+        File file = new File("/home/magnus/IdeaProjects/Offloading/data/" + name.getName() + ".txt");
+        try {
+            InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+            return ResponseEntity.ok()
+                    .headers(new HttpHeaders())
+                    .contentLength(file.length())
+                    .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                    .body(resource);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
