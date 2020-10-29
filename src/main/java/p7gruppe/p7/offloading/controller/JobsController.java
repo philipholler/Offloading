73539777e076e355bfd0fe9998c69b9804d67901
Philip Holler <philipholler94@icloud.com>
2020-10-29 11:14:40 +0100

    Correct path for jobs

--- src/main/java/p7gruppe/p7/offloading/controller/JobsController.java
+++ src/main/java/p7gruppe/p7/offloading/controller/JobsController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.multipart.MultipartFile;
 import p7gruppe.p7.offloading.api.JobsApi;
 import p7gruppe.p7.offloading.model.Job;
 
@@ -13,13 +14,16 @@ import javax.validation.constraints.NotNull;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @Controller
 public class JobsController implements JobsApi {
 
+    static final String jobsPath = System.getProperty("user.dir") + File.separator + "data";
+
     @Override
     public ResponseEntity<Resource> jobsGet(@NotNull @Valid Job name) {
-        File file = new File("/home/magnus/IdeaProjects/Offloading/data/" + name.getName() + ".txt");
+        File file = new File(jobsPath + name.getName());
         try {
             InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
             return ResponseEntity.ok()
@@ -34,9 +38,8 @@ public class JobsController implements JobsApi {
     }
 
     @Override
-    public ResponseEntity<Void> jobsPost(@NotNull @Valid Job query) {
-        System.out.println("Got post request");
-        System.out.println("Received post - id: " + query.getId() + ", name: " + query.getName());
+    public ResponseEntity<Integer> jobsPost(@Valid Resource body) {
+        System.out.println(body.getDescription());
         return null;
     }
 }
