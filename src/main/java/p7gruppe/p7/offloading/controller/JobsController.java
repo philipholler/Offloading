00dd63c38d0aa0e0c2b7cb521fad653c2e4fd562
Mads <madsfaber111@live.dk>
2020-11-02 13:08:02 +0100

    testing db

--- src/main/java/p7gruppe/p7/offloading/controller/JobsController.java
+++ src/main/java/p7gruppe/p7/offloading/controller/JobsController.java
@@ -8,12 +8,14 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartResolver;
 import p7gruppe.p7.offloading.api.JobsApi;
+import p7gruppe.p7.offloading.database.DataManager;
 import p7gruppe.p7.offloading.model.Job;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.io.*;
 import java.nio.file.Paths;
+import java.sql.SQLException;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -26,13 +28,14 @@ public class JobsController implements JobsApi {
     public ResponseEntity<Resource> jobsGet(@NotNull @Valid Job name) {
         File file = new File(jobsPath + name.getName());
         try {
+            System.out.println(DataManager.getFirstDbname());
             InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
             return ResponseEntity.ok()
                     .headers(new HttpHeaders())
                     .contentLength(file.length())
                     .contentType(MediaType.APPLICATION_OCTET_STREAM)
                     .body(resource);
-        } catch (FileNotFoundException e) {
+        } catch (FileNotFoundException | SQLException e) {
             e.printStackTrace();
         }
         return null;
--- src/main/java/p7gruppe/p7/offloading/database/QueryManager.java
+++ src/main/java/p7gruppe/p7/offloading/database/QueryManager.java
@@ -4,6 +4,6 @@ public class QueryManager {
     public static String connectionString = "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=password";
 
     public static String selectAllFromTable(){
-        return "SELECT * FROM test";
+        return "SELECT * FROM \"Clients\"";
     }
 }
