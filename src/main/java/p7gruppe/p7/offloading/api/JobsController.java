800e0ca140af78b0aaa7ca41290787a65ca97ba7
ScumbagDog <lbrack17@student.aau.dk>
2020-11-04 10:12:40 +0100

    Added simple implementation for jobscontroller

--- src/main/java/p7gruppe/p7/offloading/api/JobsController.java
+++ src/main/java/p7gruppe/p7/offloading/api/JobsController.java
@@ -6,15 +6,15 @@ import p7gruppe.p7.offloading.model.Job;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 @Controller
 public class JobsController implements JobsApi {
     @Override
-    public Optional<NativeWebRequest> getRequest() {
-        System.out.println("Got get request");
-        return Optional.empty();
+    public ResponseEntity<List<Job>> jobsGet() {
+        return ResponseEntity.ok(Collections.emptyList());
     }
 
     @Override
