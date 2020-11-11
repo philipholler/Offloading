ed31b7f833f465d573740e116017fe6207c3c7ea
Mads <madsfaber111@live.dk>
2020-11-11 11:21:14 +0100

    created Sql Methods

--- src/main/java/p7gruppe/p7/offloading/database/DataManager.java
+++ src/main/java/p7gruppe/p7/offloading/database/DataManager.java
@@ -2,22 +2,39 @@ package p7gruppe.p7.offloading.database;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class DataManager {
     static ResultSet resultSet;
 
-    public static String getFirstDbname() throws SQLException {
-        resultSet = ConnectionManager.selectSQL(QueryManager.selectAllFromTable());
+    public static void insertJobInDB(String jobName, String jobPath) {
+        ConnectionManager.updateSql(QueryManager.insertJob(jobName, jobPath));
+    }
+
+    public static List getCurrentJobs(String userName) throws SQLException {
+        resultSet = ConnectionManager.selectSQL(QueryManager.selectAllJobs(userName));
+        ArrayList<String> list = new ArrayList();
         while (resultSet.next()) {
             String name = resultSet.getString(2);
-            return name;
         }
-        return "";
-
+        return list;
     }
 
-
-    public static void insertJobInDB(String jobName, String jobPath){
-        ConnectionManager.updateSql(QueryManager.insertJob(jobName,jobPath));
+    public static void createUser(String userName, String pass) {
+        ConnectionManager.updateSql(QueryManager.createUser(userName, pass));
     }
+
+    public String selectJobResult(int jobID, String userName) throws SQLException {
+        resultSet = ConnectionManager.selectSQL(QueryManager.selectJobResult(jobID, userName));
+        while (resultSet.next()) {
+            return resultSet.getString(2);
+        }
+        return "no result found";
+    }
+
+    public void removeJob(int jobID) {
+        ConnectionManager.updateSql(QueryManager.deleteJob(jobID));
+    }
+
 }
--- src/main/java/p7gruppe/p7/offloading/database/QueryManager.java
+++ src/main/java/p7gruppe/p7/offloading/database/QueryManager.java
@@ -3,12 +3,35 @@ package p7gruppe.p7.offloading.database;
 public class QueryManager {
     public static String connectionString = "jdbc:postgresql://localhost:5432/postgres?user=postgres&password=password";
 
-    public static String selectAllFromTable(){
+    public static String selectAllFromTable() {
         return "SELECT * FROM \"Clients\"";
     }
 
 
-    public static String insertJob(String jobName, String jobPath){
-        return "INSERT INTO \"Job\" (jobname, \"Jobpath\") VALUES ('"+jobName+"', '"+jobPath+"')";
+    public static String insertJob(String jobName, String jobPath) {
+        return "INSERT INTO \"Job\" (jobname, \"Jobpath\") VALUES ('" + jobName + "', '" + jobPath + "')";
     }
+
+    public static String selectAllJobs(String username) {
+        return "SELECT jobid, name, jobpath, status, username FROM \"Jobs\"\n" +
+                "INNER JOIN \"Device\" D on D.deviceid = \"Jobs\".deviceid\n" +
+                "INNER JOIN \"Clients\" C on C.clientid = D.clientid\n" +
+                "WHERE username = '" + username + "'";
+    }
+
+    public static String createUser(String username, String pass) {
+        return "INSERT INTO \"Clients\" (username, password) VALUES ('" + username + "','" + pass + "')";
+    }
+
+    public static String selectJobResult(int jobID, String userName) {
+        return "SELECT resultpath FROM \"Jobs\" INNER JOIN \"Device\" D on D.deviceid = \"Jobs\".deviceid\n" +
+                "INNER JOIN \"Clients\" C on C.clientid = D.clientid\n" +
+                "WHERE jobid = " + jobID + " AND username = '" + userName + "'";
+    }
+
+    public static String deleteJob(int jobID) {
+        return "DELETE FROM \"Jobs\" WHERE jobid = " + jobID + "";
+    }
+
+
 }
